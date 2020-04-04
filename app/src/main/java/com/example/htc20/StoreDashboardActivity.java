7b00833466e1d6e9b3a4a84fd3ef259853773103
nswarup14 <nswarup14@gmail.com>
2020-04-04 11:13:08 +0530

    Add registration and login for the store owners

---
+++ app/src/main/java/com/example/htc20/StoreDashboardActivity.java
@@ -0,0 +1,13 @@
+package com.example.htc20;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StoreDashboardActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.store_activity_dashboard);
+    }
+}
