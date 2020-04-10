8cd0be206d4fafff319624c7a77644ffa59ba3a9
acharaakshit <acharaakshit@gmail.com>
2020-04-11 00:30:05 +0530

    removed unnecessary class

--- app/src/main/java/com/example/htc20/PlaceListActivity.java
+++ app/src/main/java/com/example/htc20/PlaceListActivity.java
@@ -2,7 +2,6 @@ package com.example.htc20;
 
 import android.annotation.SuppressLint;
 import android.app.Dialog;
-import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.net.Uri;
@@ -13,7 +12,6 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -27,7 +25,6 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -617,33 +614,6 @@ class RequestJsonPlaces extends AsyncTask<String, String, String> {
 
 }
 
-
-class NoteRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-
-    private ArrayList<String> mNotes = new ArrayList<>();
-    private Context mContext;
-
-    public NoteRecyclerViewAdapter(Context context, ArrayList<String> notes) {
-        mNotes = notes;
-        mContext = context;
-    }
-
-    @NonNull
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
-}
 
 class Point {
     double lattitude;
